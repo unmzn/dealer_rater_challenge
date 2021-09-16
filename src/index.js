89f0b702221f7a4e5b0071d9cebf090630a5ceb0
Alex Webb <webb.alexander.t@gmail.com>
2021-09-16 17:36:32 -0600

    Now outputting scores

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
-import { promises as fs } from 'fs';
-import { fileURLToPath } from 'url';
-import path, { dirname } from 'path';
 import cheerio from 'cheerio';
+import path, { dirname } from 'path';
 import pretty from 'pretty';
+import { fileURLToPath } from 'url';
+import { promises as fs } from 'fs';
+import Sentiment from 'sentiment';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 async function main() {
+    const sentiment = new Sentiment();
     const rawText = await fs.readFile(path.join(__dirname, '../data/page_snapshot.html'), 'utf8');
     const $ = cheerio.load(rawText);
     const reviews = $('p.review-content');
     reviews.each((i, elem) => {
         const reviewText = $(elem).html();
-        //console.log($(elem).html());
+        const { comparative } = sentiment.analyze(reviewText);
+        console.log({ reviewText, score: comparative });
     });
 }
 
